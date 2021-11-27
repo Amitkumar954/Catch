917a338a738467633b0f2fd1799475895774dc61
Amit <amit.kumar_cs20@gla.ac.in>
2021-11-27 23:56:09 +0530

    Initial commit

---
+++ src/CatchBlockExa.java
@@ -0,0 +1,19 @@
+public class CatchBlockExa {
+    public static void main(String[] args) {
+        // we can write the high risk code inside the try block
+        try{
+            int i=50/0;
+            // if exception occurs , the remaining statement will not excecute
+            System.out.println("Rest of the code1");
+        }
+        catch(ArithmeticException obj){ // this excption is child class exception
+            //this line is print the description of the error
+            System.out.println(obj);
+
+            // print the default message to the user.
+            System.out.println("Can't divided by zero");
+
+        }
+        System.out.println("Rest of the code2");
+    }
+}
